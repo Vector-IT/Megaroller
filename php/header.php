260de8562e324d4c35e4880aa98a92ed8eff2164
elianemeyer <elumeyer@gmail.com>
2017-09-27 17:55:33 -0300

    Login/registrarse/logout en header

--- php/header.php
+++ php/header.php
@@ -3,8 +3,30 @@
 	$twit = buscarDato("SELECT ValoConf FROM configuraciones WHERE NombConf = 'TWITTER'");
 	$inst = buscarDato("SELECT ValoConf FROM configuraciones WHERE NombConf = 'INSTAGRAM'");
 	$goog = buscarDato("SELECT ValoConf FROM configuraciones WHERE NombConf = 'GOOGLE+'");
+
+  if (!isset($orden)) $orden = "1";
+	if (!isset($subcat)) $subcat = "";
+	
+	$numeUser = isset($_COOKIE["v-commerce_numeUser"])? $_COOKIE["v-commerce_numeUser"]: '';
+	if ($numeUser != '') {
+		cargarUsuario($numeUser);
+	} else {
+		if(isset($_COOKIE["v-commerce_numeInvi"])) {
+			$_SESSION["NumeInvi"] = $_COOKIE["v-commerce_numeInvi"];
+			$_SESSION["NumeCarr"] = buscarDato("SELECT NumeCarr FROM carritos WHERE NumeEstaCarr = 1 AND NumeInvi = ". $_SESSION["NumeInvi"]);
+		}
+	}
+
+	if (isset($_SESSION["NumeCarr"])) {
+		$cantCarrito = buscarDato("SELECT COUNT(*) FROM carritosdetalles WHERE NumeCarr = ". $_SESSION["NumeCarr"]);
+	}
+	else {
+		$cantCarrito = 0;
+	}
 ?>
+
 <!-- HEADER -->
+<nav>
 <!-- Links Header -->
 <div class="links-header">
 <div class="row noMargin">
@@ -19,11 +41,11 @@
     </div>
   </div>
   <div class="col-lg-6">
-    <a href="javascript:void(0);" type="button" class="animated fadeInLeft pull-right" onclick="abrirCarrito();" style="margin-right: 20px;"><img class="img-center" src="./img/item-producto/mi-carrito.png" alt="" style="width: 25px;margin-top: 5px;"><span class="cantidad-productos cantProds">0</span></a> 
-    <a class="pull-right" href="#" > <img class="img-center" src="./img/user.png" alt="" style="width: 20px;margin-top: 8px;margin-right: 10px;"></a>
-    <p class="telefonos pull-right">0351 464 66 60 | 0351 155 228 460</p>
+        <a class="mis-compras pull-right" href="mi-carrito.php" style="margin-right: 20px;"><img class="img-center" src="./img/item-producto/mi-carrito.png"  alt="Mi Carrito" title="Mi Carrito" style="width: 25px;margin-top: 5px;"><span class="cantidad-productos cantProds"><?php echo $cantCarrito?></span></a>
+				<div id="divLogin" class="logueo pull-right" <?php echo (isset($_SESSION['NumeUser'])? 'style="display: none;"': '')?>><a href="#login-modal" data-toggle="modal" class="navbar-link" role="button"><img class="img-center" src="./img/user.png" alt="" style="width: 20px;margin-top: 8px;margin-right: 5px;" alt="Ingresar" title="Ingresar"></a></div>
+				<div id="divLogout" class="logueo pull-right" <?php echo (!isset($_SESSION['NumeUser'])? 'style="display: none;"': '')?>><a href="php/logout.php" class="navbar-link clickable" title="Cerrar sesión"> <?php echo (isset($_SESSION['NumeUser'])? $_SESSION["NombPers"]: '')?> </div>
+            <p class="telefonos pull-right">0351 464 66 60 | 0351 155 228 460</p>
     <a class="mail pull-right" href="mailto:ventas@megarollercortinas.com" target="_top">ventas@megarollercortinas.com</a>
-  </div>
 </div>
 </div>
 <div class="navbar navbar-default yamm">
@@ -65,6 +87,134 @@
 </div>
 </div>
 <!-- /.HEADER -->
+</nav>
+
+<!-- MODAL LOGIN -->
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+<div class="modal-dialog">
+	<div class="modal-content">
+		<div class="modal-header" align="center">
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+			</button>
+		</div>
+		<!-- Begin # DIV Form -->
+		<div id="div-forms">
+			<!-- Begin # Login Form -->
+			<form id="login-form" method="post">
+				<h4 class="modal-title">Iniciar Sesión</h4>
+				<div class="modal-body">
+					<input class="form-control" id="nombUser" type="text" placeholder="Usuario" required style="text-transform: none !important;">
+					<input class="form-control" id="nombPass" type="password" placeholder="Contraseña" required style="text-transform: none !important;">
+					<div class="checkbox">
+						<label>
+						<input type="checkbox" id="chkRemember"> Recordarme
+						</label>
+					</div>
+					<div id="divLoginMsg" class="alert noMargin" role="alert">
+						<span id="iconLogin" class="" aria-hidden="true"></span>&nbsp;<span id="txtLoginMsg"></span>
+					</div>
+				</div>
+				<div class="modal-footer" style="border-top: none !important;">
+					<div>
+						<button type="submit" class="btn btn-small pull-left">Iniciar Sesión</button>
+					</div>
+					<div>
+						<button id="login_lost_btn" type="button" class="btn btn-link">Recuperar contraseña</button>
+						<button id="login_register_btn" type="button" class="btn btn-link" style="color: #BCAA66 !important;">Registrarse</button>
+					</div>
+				</div>
+			</form>
+			<!-- End # Login Form -->
+			<!-- Begin | Lost Password Form -->
+			<form id="lost-form" method="post" style="display:none;">
+				<h4 class="modal-title">Recuperar Contraseña</h4>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-md-12">
+							<div id="div-lost-msg">
+								<span id="text-lost-msg">Ingrese su e-mail</span>
+							</div>
+							<input id="lost_email" class="form-control" type="text" placeholder="E-Mail" style="text-transform: none !important;" required>
+							<div id="recaptcha1"></div>
+						</div>
+					</div>
+					<div id="divLostMsg" class="alert noMargin" role="alert">
+						<span id="iconLost" class="" aria-hidden="true"></span>&nbsp;<span id="txtLostMsg"></span>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<div class="row">
+						<div class="col-md-4">
+							<button type="submit" id="btnRecuperar" class="btn btn-small pull-left">Enviar</button>
+						</div>
+						<div class="col-md-8 text-right">
+							<button id="lost_login_btn" type="button" class="btn btn-link">Iniciar Sesión</button>
+							<button id="lost_register_btn" type="button" class="btn btn-link">Registrarse</button>
+						</div>
+					</div>
+				</div>
+			</form>
+			<!-- End | Lost Password Form -->
+			<!-- Begin | Register Form -->
+			<form id="register-form" style="display:none;">
+				<h4 class="modal-title" style="">Registrarse</h4>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-md-12">
+							<label for="NombPers">Nombre completo *</label>
+							<input type="text" class="form-control form-custom" id="NombPers" placeholder="Nombre Completo *" required>
+							<label for="TeleUser">Teléfono *</label>
+							<input type="text" class="form-control form-custom" id="TeleUser" placeholder="Teléfono *" required>
+							<label for="MailUser">Mail *</label>   
+							<input type="email" class="form-control form-custom" id="MailUser" placeholder="Correo Electrónico *" required>
+							<div class="row">
+								<div class="col-md-8">
+									<label for="DireUser">Dirección *</label>
+									<input type="text" class="form-control form-custom" id="DireUser" placeholder="Dirección *" required>
+								</div>
+								<div class="col-md-4">
+									<label for="CodiPost">Código Postal *</label>
+									<input type="text" class="form-control form-custom" id="CodiPost" placeholder="Código Postal" required>
+								</div>
+							</div>
+							<label from="NombLoca">Localidad *</label>
+							<input type="text" class="form-control form-custom" id="NombLoca" placeholder="Localidad *" required>
+							<label for="NumeProv">Provincia *</label>
+							<select class="form-control form-custom" id="NumeProv" required>
+							<?php echo cargarCombo("SELECT NumeProv, NombProv FROM provincias ORDER BY NombProv", "NumeProv", "NombProv");?>
+							</select>
+							<label for="NombUser">Usuario *</label>
+							<input type="text" class="form-control form-custom" id="NombUser" placeholder="Usuario *" required>
+							<label for="NombPass">Contraseña *</label>
+							<input type="password" class="form-control form-custom" id="NombPass" placeholder="Contraseña *" required>
+							<input type="password" class="form-control form-custom" id="NombPass2" placeholder="Repetir Contraseña *" required>
+							<div id="recaptcha2"></div>
+						</div>
+					</div>
+					<div id="divRegisterMsg" class="alert noMargin" role="alert">
+						<span id="iconRegister" class="" aria-hidden="true"></span>&nbsp;<span id="txtRegisterMsg"></span>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<div class="row">
+						<div class="col-md-4">
+							<button type="submit" class="btn btn-small pull-left">Registrarse</button>  
+						</div>
+						<div class="col-md-8 text-right">
+							<p>Ya eres un usuario registrado? <button id="register_login_btn" type="button" class="btn btn-link">Iniciar sesión</button>    </p>
+						</div>
+					</div>
+				</div>
+			</form>
+			<!-- End | Register Form -->
+		</div>
+		<!-- End # DIV Form -->
+	</div>
+</div>
+</div>
+<!-- /.MODAL LOGIN -->
+
 <!-- Modal Presupuestar -->
 <div class="modal fade" id="presupuestar" tabindex="-1" role="dialog" aria-labelledby="presupuestarModalLabel" aria-hidden="true">
 <div class="modal-dialog" role="document">
@@ -105,7 +255,7 @@
                   <input type="email" class="form-control" placeholder="" id="email" required data-validation-required-message="Por favor ingresa tu email.">
                   <p class="help-block text-danger"></p>
                   <label>Teléfono</label>
-                  <input type="tel" class="form-control" placeholder="" id="phone" required data-validation-required-message="Por favor ingresa tu tel�fono.">
+                  <input type="tel" class="form-control" placeholder="" id="phone" required data-validation-required-message="Por favor ingresa tu teléfono.">
                   <p class="help-block text-danger"></p>
                   <label>Consulta</label>
                   <textarea rows="5" class="form-control" placeholder="" id="message" required data-validation-required-message="Por favor ingresa tu consulta."></textarea>
