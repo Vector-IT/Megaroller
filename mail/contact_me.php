f166808b6f66a3b0323e8a486fa1598e805dd53b
elianemeyer <elumeyer@gmail.com>
2017-09-28 17:33:28 -0300

    Detalles de colores, estilos y mails en formulario de contacto

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'info@eadvocatus.com.ar'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@megarollercortinas.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Consulta Web:  $name";
-$email_body = "Has recibido una nueva consulta desde el formulario de contacto.\n\n"."\n\nNombre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nConsulta:\n$message";
-$headers = "From: noreply@eadvocatus.com.ar\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_body = "Has recibido una nueva consulta desde el formulario de contacto.\n\n"."\n\nNombre: $name\n\nEmail: $email_address\n\nTelï¿½fono: $phone\n\nConsulta:\n$message";
+$headers = "From: noreply@megarollercortinas.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
