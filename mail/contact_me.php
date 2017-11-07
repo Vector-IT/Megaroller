42744fe5edd407a68dcadcce54e51fe669a6e30f
elianemeyer <elumeyer@gmail.com>
2017-11-07 14:53:12 -0300

    Cambio mail de form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'info@megarollercortinas.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'seabcortinasroller@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Consulta Web:  $name";
 $email_body = "Has recibido una nueva consulta desde el formulario de contacto.\n\n"."\n\nNombre: $name\n\nEmail: $email_address\n\nTel�fono: $phone\n\nConsulta:\n$message";
 $headers = "From: noreply@megarollercortinas.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
